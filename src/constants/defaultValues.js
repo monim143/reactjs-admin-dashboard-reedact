d9464d1fc35aedb091baea971a7b785e642f12ea
-global <-global>
2020-07-13 12:15:05 +0600

    initial

---
+++ src/constants/defaultValues.js
@@ -0,0 +1,46 @@
+/* 
+Menu Types:
+"menu-default", "menu-sub-hidden", "menu-hidden"
+*/
+export const defaultMenuType = 'menu-default';
+
+export const subHiddenBreakpoint = 1440;
+export const menuHiddenBreakpoint = 768;
+export const defaultLocale = 'en';
+export const localeOptions = [
+  { id: 'en', name: 'English - LTR', direction: 'ltr' },
+  { id: 'es', name: 'Español', direction: 'ltr' },
+  { id: 'enrtl', name: 'English - RTL', direction: 'rtl' },
+];
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBBksq-Asxq2M4Ot-75X19IyrEYJqNBPcg',
+  authDomain: 'mamonim-react-login.firebaseapp.com',
+  databaseURL: 'https://mamonim-react-login.firebaseio.com',
+  projectId: 'mamonim-react-login',
+  storageBucket: 'mamonim-react-login.appspot.com',
+  messagingSenderId: '216495999563',
+};
+
+export const searchPath = '/app/pages/miscellaneous/search';
+export const servicePath = 'https://monim143.github.io/';
+
+export const themeColorStorageKey = '__theme_selected_color';
+export const isMultiColorActive = true;
+export const defaultColor = 'light.purplemonster';
+export const isDarkSwitchActive = true;
+export const defaultDirection = 'ltr';
+export const themeRadiusStorageKey = '__theme_radius';
+export const isDemo = true;
+export const colors = [
+  'bluenavy',
+  'blueyale',
+  'blueolympic',
+  'greenmoss',
+  'greenlime',
+  'purplemonster',
+  'orangecarrot',
+  'redruby',
+  'yellowgranola',
+  'greysteel',
+];
